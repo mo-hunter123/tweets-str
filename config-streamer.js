a41a2f809c5f5c0737b6929570506c3225cbdbf3
ihssane <ihatim@ihatim-ltmyinh.internal.salesforce.com>
2021-11-02 02:02:34 +0100

    use env (uncomment related code)

--- config-streamer.js
+++ config-streamer.js
@@ -4,18 +4,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 
-// const twitter = new Twitter({
-//     consumer_key: process.env.API_KEY, 
-//     consumer_secret: process.env.API_SECRET_KEY, 
-//     token: process.env.ACCESS_TOKEN,
-//     token_secret: process.env.ACCESS_TOKEN_SECRET
-// })
-
 const twitter = new Twitter({
-    consumer_key: "FKoqQuXwhkSHLcyxQNTlvNqVr", 
-    consumer_secret: "KqQqYkFmLGhi2I9xm2IavPZ0UbFcWvCyjOsuz3bGjjZNkODdfx", 
-    token: "4856955591-IpR5KRtj0RmXoc0hxLeaYfgWfpHpssGwEWm0ITE",
-    token_secret: "S8TbXHevhZ4TCYo32HgABsx2qsRVOYHn4GIPnpaqzcSpV"
+    consumer_key: process.env.API_KEY, 
+    consumer_secret: process.env.API_SECRET_KEY, 
+    token: process.env.ACCESS_TOKEN,
+    token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
 // twitter.track('socket.io')
