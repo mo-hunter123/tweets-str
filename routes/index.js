7bd4f8f0779dafabd633e416befb2e474f188d78
ihssane <ihatim@ihatim-ltmyinh.internal.salesforce.com>
2021-11-02 01:53:25 +0100

    update views architecture

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
+/// TODO : titles in a separated file for consts
+
 const express = require('express')
 
 const router = express.Router()
 
-
 router.get('/', (req, res) => {
-   res.render('../views/index', {
-       title: "Tweets streamer"
+   res.render('../views/pages/home-page', {
+       title: "Tweets streamer - Home"
    })
 })
 
+router.get('/user/:screen_name', (req, res) => {
+    const username = req.params.screen_name;
+    res.render('../views/pages/user-page', {
+        title: `Tweets streamer - ${username}`,
+        username
+    })
+ })
 
 module.exports = router
